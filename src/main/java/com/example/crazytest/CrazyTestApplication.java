5ec34404851d6f07c01c2ed5b36c60222dc4ae09
menghuiding <dingmenghui026@163.com>
2025-03-07 19:59:17 +0800

    feat: init

---
+++ src/main/java/com/example/crazytest/CrazyTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.crazytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrazyTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CrazyTestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/crazytest/CrazyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crazytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrazyTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
