477353e0bbd9655743c0f2fff728057a4b0d77f0
menghuiding <dingmenghui026@163.com>
2025-04-01 01:15:31 +0800

    fix: xxl -job update config

--- src/main/java/com/example/crazytest/config/XxlJobConfig.java
+++ src/main/java/com/example/crazytest/config/XxlJobConfig.java
@@ -15,17 +15,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class XxlJobConfig {
 
-  @Value("${xxl.job.admin.addresses:}")
+  @Value("${xxl.job.admin.addresses}")
   private String adminAddresses;
 
-  @Value("${xxl.job.accessToken:}")
+  @Value("${xxl.job.accessToken}")
   private String accessToken;
 
   @Value("${xxl.job.executor.appname}")
   private String appname;
 
-//  @Value("${xxl.job.executor.address}")
-//  private String address;
+  @Value("${xxl.job.executor.address}")
+  private String address;
 
   @Value("${xxl.job.executor.ip}")
   private String ip;
@@ -39,14 +39,13 @@ public class XxlJobConfig {
   @Value("${xxl.job.executor.logretentiondays}")
   private int logRetentionDays;
 
-  @Value("${xxl.job.executor.logretentiondays}")
 
   @Bean
   public XxlJobSpringExecutor xxlJobExecutor() {
     XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
     xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
     xxlJobSpringExecutor.setAppname(appname);
-//    xxlJobSpringExecutor.setAddress(address);
+    xxlJobSpringExecutor.setAddress(address);
     xxlJobSpringExecutor.setIp(ip);
     xxlJobSpringExecutor.setPort(port);
     xxlJobSpringExecutor.setAccessToken(accessToken);
